102c50b6cafca32d1e129d32d460b2687ff26ea7
Babarao <raometkar@gmail.com>
2024-03-07 00:56:46 +0530

    Commiting first code of loginPage

---
+++ src/testPackage/LoginPageClassForProject.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class LoginPageClassForProject {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the username");
+		
+		System.out.println("Enter the password");
+	}
+}
